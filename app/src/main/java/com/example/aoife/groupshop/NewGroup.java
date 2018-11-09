ee5efd8cfbd402fbd138f52d038407b3fec77a84
CrappyRaizur <razizo41@hotmail.com>
2018-11-08 18:28:41 -0700

    Fixed new group class file

--- app/src/main/java/com/example/aoife/groupshop/NewGroup.java
+++ app/src/main/java/com/example/aoife/groupshop/NewGroup.java
@@ -1,4 +1,13 @@
 package com.example.aoife.groupshop;
 
-public class NewGroup {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newgroup);
+    }
 }
