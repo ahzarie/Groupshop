6aba0729c348684f2e91bef1d02ce41b4f0d4747
CrappyRaizur <razizo41@hotmail.com>
2018-12-02 22:07:06 -0700

    Tested functionality to My groups class

--- app/src/main/java/com/example/aoife/groupshop/NewGroup.java
+++ app/src/main/java/com/example/aoife/groupshop/NewGroup.java
@@ -3,6 +3,7 @@ package com.example.aoife.groupshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class NewGroup extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class NewGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newgroup);
+        Button createGroup = findViewById(R.id.CreateGroup);
         Spinner paymentPlan = findViewById( R.id.payment_newGroups );
         paymentPlan.setAdapter( new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item, Group.PLAN_NAMES ));
 
